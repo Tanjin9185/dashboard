199ba0daeb7f4a5dd134110d786459092ea43cf0
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2021-07-24 18:41:45 +0600

    table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
+import Department from './Components/Department/Department';
+import Table from './Components/Table/Table';
+import Pagination from './Components/Table/Pagination';
+import ActiveAdmission from './Components/ActiveAdmission/ActiveAdmission';
 
 function App() {
   return (
     <div className="App">
       <Sidebar></Sidebar>
+      <Department></Department>
+      <ActiveAdmission></ActiveAdmission>
+      <Pagination></Pagination>
+      <Table></Table>
     </div>
   );
 }
